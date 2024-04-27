66851cf51256c60e4b1b341f4cde501ca62fb577
Cpun077 <cadenpun@gmail.com>
2024-04-27 14:48:54 -0700

    basic flask connection on frontend
    run python3 server.py in venv, npm run dev in client

--- server/server.py
+++ server/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({
-        'message':'Hello World'
+        'message':'Flask Connected!'
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
