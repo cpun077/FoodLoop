3cb67624e684ccb3596a60bf008eadfe2a7483d5
Cpun077 <cadenpun@gmail.com>
2024-04-27 14:05:43 -0700

    Merge branch 'main' of https://github.com/cpun077/hackdavis24

---
+++ backend/user.py
@@ -0,0 +1,9 @@
+class User:
+	def __init__(self, name, id, address, phone_number, organization):
+
+		self.name = name
+		self.id = x_id
+		self.address
+		self.phone_number
+		self.organization = organization
+
