481ad2ed4c2b9ed8e9753d92390327d10a6d1d57
vthennarasu <156245675+vthennarasu@users.noreply.github.com>
2024-04-27 19:57:09 -0700

    Update volunteer.py

--- server/volunteer.py
+++ server/volunteer.py
@@ -3,16 +3,6 @@ import geopy.distance
 from geopy.geocoders import Nominatim
 
 
-def get_coordinates(address):
-    geolocator = Nominatim(user_agent="app")
-    location = geolocator.geocode(address)
-    if location == None:
-        return -1
-    return (location.latitude, location.longitude)
-
-def get_distance(c1, c2):
-    return geopy.distance.geodesic(c1, c2).miles
-
 class Volunteer(User):
     def __init__(self, name, email, address, city, state, zip_code, is_organization):
         super().__init__(name, email, address, city, state, is_organization)
