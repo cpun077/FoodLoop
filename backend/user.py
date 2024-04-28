1cc5f9d3a66c3f8a23df6f4fccd33eb616d1f8e6
Lumijek <lumijekpr@gmail.com>
2024-04-27 17:00:54 -0700

    Rever "restructured backend/server"
    This reverts commit 1d7e6f0fafff35bc3691dd07a4207d7c9b3368d1.

---
+++ backend/user.py
@@ -0,0 +1,12 @@
+
+class User:
+    def __init__(self, name, email, phone, address, city, state, zipcode, user_type, is_organization):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode= zipcode
+        self.user_type = user_type
+        self.is_organization = is_organization
